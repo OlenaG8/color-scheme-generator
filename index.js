dc75fad6b2bd08583d36404cb18023d1add6f4de
Olena <135965492+OlenaG8@users.noreply.github.com>
2024-03-13 23:07:55 +0100

    Design update

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-fetch ("https://www.thecolorapi.com")
-    .then(res => res.json())
-    .then(data => console.log(data))
+const baseColorInput = document.getElementById("input-color")
+let baseColor = baseColorInput.value
+
+const generateBtn = document.getElementById("generate-btn")
 
 const colorSchemesOptArr = [ 
     "Monochrome", "Monochrome-dark", "Monochrome-light", 
@@ -17,5 +18,28 @@ function renderColorSchemes() {
     }).join('')
 
 }
+
+baseColorInput.addEventListener("change", e => baseColor = e.target.value)
+
+generateBtn.addEventListener("click", function(){
+    console.log(baseColor)
+    const colorScheme = document.getElementById("color-scheme").value 
+
+    // Hex value is supossed to be put in the url without # - to fix
+    
+    if (baseColor && colorScheme) {
+        fetch(`https://www.thecolorapi.com//scheme?hex=${baseColor}&mode=triad&count=6`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data.colors)
+            })
+    } else if (baseColor) {
+        fetch(`https://www.thecolorapi.com/?hex=${baseColor}`)
+            .then(res => res.json())
+            .then(data => {
+                console.log(data.colors)
+            })
+    } else { alert("Nope") }
+})
 
 renderColorSchemes()
